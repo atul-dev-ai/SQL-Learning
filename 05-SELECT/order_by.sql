7ba46dd265605d855623602763017c8d4399b253
atul-dev-ai <paulatul020@gmail.com>
2026-08-29 23:10:37 +0600

    order name changed

---
+++ 05-SELECT/order_by.sql
@@ -0,0 +1,21 @@
+
+SELECT * FROM employees
+ORDER BY salary ASC;
+
+SELECT * FROM
+employees
+ORDER BY salary DESC;
+
+SELECT name, salary
+FROM employees
+ORDER BY salary ASC;
+
+SELECT * FROM
+employees
+ORDER BY name ASC;
+
+SELECT name, salary, department
+FROM employees
+WHERE department = 'HR'
+ORDER BY salary DESC;
+
