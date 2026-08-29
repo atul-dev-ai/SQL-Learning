82d9ff4537c9df42becd0b18e55a4a77477262c5
atul-dev-ai <paulatul020@gmail.com>
2026-08-29 22:10:05 +0600

    update sql practice

--- 04-DML/update.sql
+++ 04-DML/update.sql
@@ -11,3 +11,16 @@ SET salary = salary * 1.10
 WHERE department = 'CEO';
 
 SELECT * FROM employees;
+
+SELECT * FROM employees
+WHERE department = 'CEO';
+
+UPDATE employees
+SET salary = salary * 1.90
+WHERE department = 'CEO'
+AND salary < 150000;
+
+UPDATE employees
+SET salary = salary * 0.8
+WHERE department = 'CEO';
+
