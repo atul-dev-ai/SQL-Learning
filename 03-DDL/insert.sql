82501f92bfb148d17730a8e4673840a3835099d8
atul-dev-ai <paulatul020@gmail.com>
2026-08-29 20:10:05 +0600

    insertion practice

---
+++ 03-DDL/insert.sql
@@ -0,0 +1,9 @@
+USE coachingmanagement;
+
+INSERT INTO courses (course_code, course_name, credit)
+VALUES
+('CIS216', 'Object Oriented Programming', 3),
+('CIS122', 'Computer Network', 3),
+('CIS111', 'Accounting', 3);
+
+SELECT * FROM courses;
