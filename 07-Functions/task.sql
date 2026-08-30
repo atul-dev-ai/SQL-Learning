9fa408f1a9c91da73ea8e35d586adf3fefc0f49d
atul-dev-ai <paulatul020@gmail.com>
2026-08-30 19:42:01 +0600

    task complete

--- 07-Functions/task.sql
+++ 07-Functions/task.sql
@@ -1,2 +1,29 @@
 
 SELECT COUNT(*) FROM employees;
+
+SELECT SUM(salary) AS total_salary
+FROM employees;
+
+SELECT AVG(salary) AS avg_salary
+FROM employees;
+
+SELECT MAX(salary) as highest_salary
+FROM employees;
+
+SELECT MIN(salary) AS minimum_slry
+FROM employees;
+
+SELECT * FROM employees
+WHERE department = 'HR';
+
+SELECT SUM(salary) AS total_salary
+FROM employees
+WHERE department = 'HR';
+
+SELECT COUNT(*) AS total_employee,
+    SUM(salary) AS total_salary,
+    AVG(salary) AS average_salary,
+    MAX(salary) AS highest_salary,
+    MIN(salary) AS minimun_salary
+FROM employees
+WHERE salary > 40000;
