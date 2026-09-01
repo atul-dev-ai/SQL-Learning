29c95da12240702651c086620f2e970dab8db2a1
atul-dev-ai <paulatul020@gmail.com>
2026-09-01 20:08:49 +0600

    having practive

---
+++ 08-JOIN/goup-by-join.sql
@@ -0,0 +1,29 @@
+
+SELECT
+    d.name AS department_name,
+    COUNT(e.id) AS total_employees
+FROM departments d
+JOIN employees e
+ON d.id = e.department_id
+GROUP BY d.id, d.name;
+
+SELECT
+    d.name AS department_name,
+    COUNT(e.id) AS total_employees,
+    SUM(e.salary) AS total_salary,
+    AVG(e.salary) AS average_salary,
+    MAX(e.salary) AS maximum_salary,
+    MIN(e.salary) AS lowest_salary
+FROM departments d 
+JOIN employees e 
+ON d.id = e.department_id
+GROUP BY d.id, d.name;
+
+SELECT
+    d.name AS department_name,
+    COUNT(e.id) AS total_employees
+FROM departments d 
+JOIN employees e 
+ON d.id = e.department_id
+GROUP BY d.id, d.name
+HAVING COUNT(e.id) >= 2;
